13cc4bb11140b91a775a487482a022b49710bd84
Ishita Dutta <ishita130602@gmail.com>
2025-10-19 23:11:42 +0530

    Created Post model

---
+++ backend/models/Post.js
@@ -0,0 +1,85 @@
+import mongoose from "mongoose";
+
+const postSchema=new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+            trim: true,
+        },
+        subtitle:{
+            type: String,
+            trim: true,
+        },
+        content:{
+            type: String,
+            required: true,
+        },
+        coverImage:{
+            type: String,
+        },
+        readingTime:{
+            type: Number,
+        },
+        tags:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Tags",
+            }
+        ],
+
+        author:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        authorName:{
+            type: String,
+        },
+        authorProfilePic:{
+            type: String,
+        },
+
+        likes:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Likes",
+            }
+        ],
+        comments:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Comments",
+            }
+        ],
+        likeCount:{
+            type: Number,
+            default: 0,
+        },
+        commentCount:{
+            type: Number,
+            default: 0,
+        },
+
+        slug:{  //generate URL by title 
+            type: String,
+            required: true,
+            unique: true,
+        },
+        status:{
+            type: String,
+            enum: ["draft","published"],
+            default: "draft",
+        },
+    },
+    {timestamps:true}
+);
+
+postSchema.pre("validate",function(next){
+    if (this.title && !this.slug){
+        this.slug=this.title.trim().toLowerCase().replace(/[^\w\s-]/g,"").replace(/\s+/g,"-");
+    }
+    next();
+});
+
+module.exports=mongoose.model("Post",postSchema);
