ed50819a96ba00f214c9efe9082f1d8dcc0d0a66
Ishita Dutta <ishita130602@gmail.com>
2025-10-19 23:27:30 +0530

    Created comment model

---
+++ backend/models/Comment.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const commentSchema=new mongoose.Schema(
+    {
+        post:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            required: true,
+        },
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true, 
+        },
+        parentComment:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+            default: null,
+        },
+        content:{
+            type: String,
+            trim: true,
+            required: true,
+        },
+        likes:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Like",
+            }
+        ],
+        likeCount:{
+            type: Number,
+            default: 0,
+        },
+    },
+    {timestamps:true}
+);
+
+module.exports=mongoose.model("Comment",commentSchema);
