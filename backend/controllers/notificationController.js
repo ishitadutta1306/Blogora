94f0374804d3ffd0ddc966fe8fffdf736d0ca393
Ishita Dutta <ishita130602@gmail.com>
2025-10-30 23:08:11 +0530

    Created notificationController

---
+++ backend/controllers/notificationController.js
@@ -0,0 +1,32 @@
+import Notification from '../models/Notification'
+
+//Get all notifications for a user
+export const getAllNotifications=async(req,res)=>{
+    try{
+        const notifications=await Notification.findById({recipient: req.user.id})
+            .sort({createdAt: -1})
+            .populate("sender","username profilePic")
+            .populate("post","title")
+            .populate("comment","content");
+        
+        res.status(200).json(notifications);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({message: "Server error"});
+    }
+}
+
+//Mark notification as read
+export const markAsRead=async(req,res)=>{
+    try{
+        //find notification & mark it as read 
+        await Notification.findById(req.params.id,{isRead: true});
+
+        res.status(200).json({message: "Notification marked as read"});
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({message: "Server error"});
+    }
+}
