77ce1c7b02ae8f771b286ada0d803a504ba7112e
Ishita Dutta <ishita130602@gmail.com>
2025-11-01 22:29:45 +0530

    Mount userRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
-require('dotenv').config();
+import express from 'express'
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import userRoutes from './routes/userRouter.js'
 
 const app=express();
 const PORT=process.env.PORT || 5000;
@@ -9,6 +12,9 @@ const PORT=process.env.PORT || 5000;
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+//Routes:
+app.use('/api/users',userRoutes);
 
 //Basic route
 app.get('/',(req,res)=>{
