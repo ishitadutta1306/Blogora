0c4dc912ab801563956fa1e8d83f5384829ee968
Ishita Dutta <ishita130602@gmail.com>
2025-11-17 23:24:35 +0530

    Designed navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { Menu, Search, SquarePen, Bell, CircleUserRound } from 'lucide-react'
+import logo from '../assets/logo.png';
+import { useState } from 'react';
+import Sidebar from './Sidebar';
+
+const Navbar=()=>{
+    const [isSidebarOpen, setIsSidebarOpen]=useState(false);
+
+    const toggleSidebar=()=>{
+        setIsSidebarOpen(prev=>!prev);
+    }
+
+    return(
+        <>
+            {/* Navbar container */}
+            <nav className='flex justify-between items-center px-8 border-b border-[#F1F1F1]'>
+                {/* Left side */}
+                <div className='flex items-center'>
+                    <button onClick={toggleSidebar} className='hover:cursor-pointer'>
+                        <Menu className='h-6 w-6'/>
+                    </button>
+                    <img src={logo} alt='logo' className='w-10 h-10 md:w-16 md:h-16 object-contain'/>
+                    <h1 className='font-bold text-lg md:text-xl'>Blogora</h1>
+                </div>
+
+                {/* Center */}
+                <div className='hidden sm:flex items-center h-10 w-full max-w-[180px] md:max-w-md px-3 border border-black rounded-3xl mx-4'>
+                    <Search className='text-black'/>
+                    <input type="text" placeholder='Search' className='ml-2 w-full outline-none text-md placeholder-gray-600'/>
+                </div>
+                {/* Mobile Search Icon (visible only below sm) */}
+                <button className="sm:hidden">
+                    <Search className="h-6 w-6"/>
+                </button>
+
+                {/* Right side */}
+                <div className='flex items-center gap-4 [&>*]:cursor-pointer'>   {/* [&>*]:apply cursor-pointer to all children */}
+                    <div className='flex items-center gap-1'>
+                        <SquarePen/>
+                        <span className='font-medium hidden md:inline'>Write</span>
+                    </div>
+                    <Bell/>
+                    <CircleUserRound/>
+                </div>  
+            </nav>
+
+            <Sidebar isOpen={isSidebarOpen} onClose={()=>setIsSidebarOpen(false)}/>
+        </>
+    );
+}
+
+export default Navbar;
