686cae3c41dd7a51ee7921759ac27036060e25ff
Ishita Dutta <ishita130602@gmail.com>
2025-11-17 23:29:00 +0530

    Imported homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 // import { useState } from 'react'
-
+import Homepage from './pages/Homepage'
 
 function App() {
 
   return (
-    <>
-      <h1 className='font-bold text-purple-700'>Hello world</h1>
-    </>
+    <div>
+      <Homepage/>
+    </div>
   )
 }
 
