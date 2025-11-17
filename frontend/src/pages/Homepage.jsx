00ad36dfa4444cef7411ca37d1b80d2283de8837
Ishita Dutta <ishita130602@gmail.com>
2025-11-17 23:28:07 +0530

    Imported navbar & sidebar

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+const Homepage=()=>{
+    return(
+        <>
+            <Navbar/>
+            <Sidebar/>
+        </>
+
+    );
+}
+
+export default Homepage;
