f175422594c7d77142c35862f30483dbda7df3db
Ishita Dutta <ishita130602@gmail.com>
2025-11-22 22:42:05 +0530

    Imported blog card component

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,13 +1,27 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import BlogCard from "../components/BlogCard";
 
 const Homepage=()=>{
     return(
         <>
             <Navbar/>
             <Sidebar/>
-        </>
 
+            <div className="flex justify-center">
+                <div className="w-full px-4 sm:px-2 pt-20 flex flex-col items-center space-y-6">
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                </div>
+            </div>
+        </>
     );
 }
 
