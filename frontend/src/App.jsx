dd7437c452eaf028fb73dc667e25a792b20be563
Ishita Dutta <ishita130602@gmail.com>
2025-12-03 23:32:43 +0530

    Added react router setup with routes for sign up, login & landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-// import { useState } from 'react'
-import Homepage from './pages/Homepage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import Register from './pages/Register'
+import Login from './pages/Login'
+// import Homepage from './pages/Homepage'
 
 function App() {
 
   return (
-    <div>
-      <Homepage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        {/* <Route path='/home' element={<Homepage/>}/> */}
+      </Routes>
+    </Router>
   )
 }
 
