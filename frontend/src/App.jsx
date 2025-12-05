e12c9c7da6bcf6bd84e5fba29916880c181f5655
Ishita Dutta <ishita130602@gmail.com>
2025-12-05 23:28:40 +0530

    Setup routing with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import Register from './pages/Register'
 import Login from './pages/Login'
-// import Homepage from './pages/Homepage'
+import Homepage from './pages/Homepage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/login' element={<Login/>}/>
-        {/* <Route path='/home' element={<Homepage/>}/> */}
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Public routes */}
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+
+      {/* Protected routes */}
+      <Route 
+        path='/home' 
+        element={
+          <ProtectedRoute>
+            <Homepage/>
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   )
 }
 
