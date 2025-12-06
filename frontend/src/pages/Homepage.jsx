ef6bc2da7d589f5f4098462e13abe5418dd39143
Ishita Dutta <ishita130602@gmail.com>
2025-12-06 23:47:02 +0530

    Fetch posts from backend

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,24 +1,41 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import BlogCard from "../components/BlogCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Homepage=()=>{
+    const [posts, setPosts]=useState([]);
+
+    useEffect(()=>{
+        const fetchPosts=async()=>{
+            try{
+                const res=await axios.get("http://localhost:5000/api/posts");
+                setPosts(res.data);   //array of formatted posts
+            }
+            catch(err){
+                console.error("Error fetching posts: ",err);
+            }
+        }
+
+        fetchPosts();
+    },[]);
+
     return(
         <>
             <Navbar/>
             <Sidebar/>
 
+            {/* Blogs list */}
             <div className="flex justify-center">
                 <div className="w-full px-4 sm:px-2 pt-20 flex flex-col items-center space-y-6">
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
+                    {posts.length===0 ? (
+                        <p className="font-medium">No posts available</p>
+                    ) : (
+                        posts.map((post)=>
+                            <BlogCard key={post.id} post={post}/>    
+                        )
+                    )}
                 </div>
             </div>
         </>
