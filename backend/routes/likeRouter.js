1af3ea5c4b67ad44638af9afa7a99db4a5d37b18
Ishita Dutta <ishita130602@gmail.com>
2025-12-16 22:53:14 +0530

    Added route for fetching likes on posts

--- backend/routes/likeRouter.js
+++ backend/routes/likeRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { toggleLikePost, toggleLikeComment } from "../controllers/likeController.js";
+import { toggleLikePost, getPostLikes, toggleLikeComment } from "../controllers/likeController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router=express.Router();
 
 //only protected routes:
 router.post("/post/:postId",protect,toggleLikePost);
+router.get("/post/:postId/likes", protect, getPostLikes);
 router.post("/comment/:commentId",protect,toggleLikeComment);
 
 export default router;
