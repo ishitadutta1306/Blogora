2f67097cbcb32d74c67ede367c11cb27992821e1
Ishita Dutta <ishita130602@gmail.com>
2025-12-24 22:52:10 +0530

    Created routes for search functionality

---
+++ backend/routes/searchRouter.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { globalSearch } from "../controllers/searchController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router=express.Router();
+router.get("/", protect, globalSearch);
+
+export default router;
